5b72be305de403ec05b9925eecf6255f4e993402
Kenichi Asai <asai@is.ocha.ac.jp>
2020-06-01 20:51:34 +0900

    add buttons for ekimei_t, etc. in class2020

--- demos/class2020/typed.js
+++ demos/class2020/typed.js
@@ -340,13 +340,10 @@ Typed.newToplevel = function() {
 Typed.onClickConvert = function(event) {
   event.preventDefault();
   var input = document.querySelector(".ocamlCode");
-  /*
   var check1 = document.querySelector(".checkbox1").checked;
   var check2 = document.querySelector(".checkbox2").checked;
   var check3 = document.querySelector(".checkbox3").checked;
-  */
   var code = '';
-  /*
   if (check1) {
     code += 'type ekimei_t = {\n' +
             '  kanji   : string; (* 漢字の駅名 *)\n' +
@@ -374,7 +371,6 @@ Typed.onClickConvert = function(event) {
             '  temae_list   : (string * string) list; (* 手前リスト *)\n' +
             '}\n'
   }
-  */
   code += input.value;
   if (code) {
     BlockOfOCamlUtils.codeToBlock(code);
