e8f2d1b1b1ae9aba0ac10b0eb1ab2954b3e15fe3
Kenichi Asai <asai@is.ocha.ac.jp>
2021-09-15 11:10:01 +0900

    add update_docs_class2020/universe

---
+++ update_docs_class2020.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python2.7
+# Update the demo page in docs4/ directory.
+
+# - The source path must be relative to the current directory.
+# - The base name of the source path is same with the destination filename by
+#   default.
+sourceMap = {
+  "block_of_ocaml_utils.js": "block_of_ocaml/utils.js",
+  "converter.js": "block_of_ocaml/",
+  "blockly_compressed.js": "./",
+  "blocks_compressed.js": "./",
+  "typedlang_compressed.js": "./",
+  "en.js": "msg/js/",
+  "ja.js": "msg/js/",
+  "index.html": "demos/class2020/dev.html",
+  "style.css": "demos/class2020/",
+  "typed.js": "demos/class2020/",
+  "eval.js": "demos/class2020/",
+}
+
+import os, shutil
+
+def verify_sourcepath():
+  for filename, source_path in sourceMap.items():
+    basename = os.path.basename(source_path)
+    if not basename:
+      sourceMap[filename] += filename
+
+def copy_files():
+  for filename, src in sourceMap.items():
+    shutil.copy(src, 'docs4/' + filename)
+
+def edit_copied_files():
+  disable_devmode()
+  # TODO(harukam): Edit dev.html to load compressed JS files in docs4/ instead
+  # of uncompressed ones.
+  pass
+
+import re
+
+def disable_devmode():
+  path = 'docs4/typed.js'
+  if not os.path.isfile(path):
+    raise Exception('Not found script: ' + path)
+
+  pattern = r'^(Typed\.DEVMODE = )(true|false);'
+  replacement = r'\1false;'
+
+  with open(path, 'r+') as f:
+    # Find the statement `Typed.DEVMODE = (true|false);` in the script.
+    body = f.read()
+    m = re.search(pattern, body, flags=re.MULTILINE)
+    if not m:
+      raise Exception('Not found DEVMODE statement')
+
+    # Disable the debugging mode if it's enabled.
+    if m.group(2) == 'true':
+      replaced_body = re.sub(pattern, replacement, body, 1, flags=re.MULTILINE)
+      f.seek(0)
+      f.truncate()
+      f.write(replaced_body)
+
+if __name__ == "__main__":
+  verify_sourcepath()
+  copy_files()
+  edit_copied_files()
