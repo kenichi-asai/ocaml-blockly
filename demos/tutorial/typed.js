61d05a6f28fdd82806b4f39103a593e746bd6250
makotoshibata <g1820517@is.ocha.ac.jp>
2021-11-01 04:16:54 +0900

    条件文チュートリアル追加

--- demos/tutorial/typed.js
+++ demos/tutorial/typed.js
@@ -106,9 +106,7 @@ Typed.init = function() {
       Typed.getWorkspaceOptions_());
   onresize();
   Blockly.svgResize(Typed.workspace);
-    console.log("hello");
     Tutorial.main();
-    console.log("bye");
 };
 
 Typed.setDocumentTitle_ = function() {
